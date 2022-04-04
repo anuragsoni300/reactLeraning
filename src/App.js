3fbe5e6706c79f6cd28a5285c650cb7b7c272f02
Anurag Soni <anurag.soni@indiamart.com>
2022-04-04 14:02:21 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Textform from "./components/Textform";
+// import Textform from "./components/Textform";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" />
       <div className="container">
-        <Textform heading="Enter the text to analyze" />{" "}
-      </div>    
+        {/* <Textform heading="Enter the text to analyze" />{" "} */}
+        <About></About>
+      </div>
     </>
   );
 }
