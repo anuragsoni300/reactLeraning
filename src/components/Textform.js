e4192c19d16f9c138b5ed914e114f45b76e30dbb
Anurag Soni <anurag.soni@indiamart.com>
2022-04-04 10:34:28 +0530

    re

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   const handleOnChange = (event) => {
     console.log(event);
     setText(event.target.value);
@@ -9,24 +9,49 @@ export default function Textform(props) {
   const handleUpClick = () => {
     setText(text.toUpperCase());
   };
+  const handleLOClick = () => {
+    setText(text.toLocaleLowerCase());
+  };
+  const handleClearClick = () => {
+    const newText = "";
+    setText(newText);
+  };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <label htmlFor="myBox" className="form-label">
-          Example textarea
-        </label>
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="1"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <label htmlFor="myBox" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="1"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          convert to upper case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLOClick}>
+          convert to lower case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          clear
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        convert to upper case
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your text summery</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>Can be read in {0.008 * text.length}</p>
+        <h2>Preview</h2>
+
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
